254ecc1fa190949052cbf257d264455af4c5e955
Intel@KALPANA <yogeshpawale633@gmail.com>
2023-07-13 23:27:24 +0530

    saves

---
+++ src/encapsulation/EncapsulationTest.java
@@ -0,0 +1,10 @@
+package encapsulation;
+
+public class EncapsulationTest {
+
+	public static void main(String[] args) {
+		Encapsulation1 obj = new Encapsulation1();
+		obj.setSalary(-5000);
+		System.out.println("salary :" +obj.getSalary());
+	}
+}
